9c5f18b8ee2161526ce3f8a27ae6132771361027
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2021-11-06 16:01:21 +0100

    +2 updates
    - Changed content of a lecture
    - Added previous and next button to a lecture

--- js/make/lecture.js
+++ js/make/lecture.js
@@ -1,15 +1,13 @@
 onloadFunc.push(function(event) {
-	var exerciseLink = document.querySelector("link[rel=exercise]");
-	if (exerciseLink == null) {
-		exerciseLink = `<div id="exercise">There's no exercise for this lecture.</div>`;
-	}
-	else {
-		exerciseLink = `
-			<div id="exercise">
-				Exercise link: <a href="${exerciseLink.getAttribute("href")}">click here</a>.
-			</div>
-		`;
-	}
+	var exerciseLink = document.querySelector("link[rel=exercise]"),
+		previousLecture = document.querySelector("link[rel=prev-lec]"),
+		nextLecture = document.querySelector("link[rel=next-lec]");
+	if (exerciseLink == null) {exerciseLink = `<div id="exercise">There's no exercise for this lecture.</div>`;}
+	else {exerciseLink = `<div id="exercise">Exercise link: <a href="${exerciseLink.getAttribute("href")}">click here</a>.</div>`;}
+	if (previousLecture == null) {previousLecture = ``;}
+	else {previousLecture = `<div id="prev-lecture"><a class="hidden" href="${previousLecture.getAttribute("href")}">Previous</a></div>`;}
+	if (nextLecture == null) {nextLecture = ``;}
+	else {nextLecture = `<div id="next-lecture"><a class="hidden" href="${nextLecture.getAttribute("href")}>Next</a></div>`;}
 	document.body.innerHTML = `
 	<div id="article">
 		<div id="title" onclick="
@@ -19,10 +17,10 @@ onloadFunc.push(function(event) {
 		">${document.title}</div>
 		${document.body.innerHTML}
 	</div>
-	${exerciseLink}
 	<div id="make-longer"></div>
+	${exerciseLink}
+	${previousLecture}
+	${nextLecture}
 	`;
-	exerciseLink = document.querySelector("link[rel=exercise]");
-	if (exerciseLink != null) exerciseLink.remove();
 	document.title += " - LearnAndShare";
 });
