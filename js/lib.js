9b2c32d08e79498b99bbfdc73193636565cbf7bb
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2021-11-06 12:52:51 +0100

    Changed favicon adding with lib.js
    So now, sources won't have favicon, but every page now needs to have lib.js imported

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,12 @@
 var onloadFunc = new Array();
+onloadFunc.push(function(event) {
+	document.head.innerHTML += `
+		<link rel="apple-touch-icon" sizes="180x180" href="https://entityplantt.github.io/LearnAndShare/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="https://entityplantt.github.io/LearnAndShare/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="https://entityplantt.github.io/LearnAndShare/favicon/favicon-16x16.png">
+		<link rel="manifest" href="https://entityplantt.github.io/LearnAndShare/favicon/site.webmanifest">
+	`;
+});
 window.onload = function(event) {
 	for (var i = 0; i < onloadFunc.length; i++) {
 		onloadFunc[i](event);
